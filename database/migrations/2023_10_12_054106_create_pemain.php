954b9a4c226316615f8747c8cd3ec3d2c0a72c27
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-10-12 12:46:30 +0700

    Added migration and Controller

--- database/migrations/2023_10_12_054106_create_pemain.php
+++ database/migrations/2023_10_12_054106_create_pemain.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('pemain', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_pemain');
+            $table->integer('no_punggung');
+            $table->string('posisi');
             $table->timestamps();
         });
     }
