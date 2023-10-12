c9b6e2c8518c79cd29f55c4e8c200ea4e560ac38
senaajibayumurti <senaajibayumurti@mail.ugm.ac.id>
2023-10-12 13:57:56 +0700

    created PemainController

--- app/Http/Controllers/PemainController.php
+++ app/Http/Controllers/PemainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pemain;
 use Illuminate\Http\Request;
 
 class PemainController extends Controller
 {
-    //
+    public function index(){
+        $data_pemain = Pemain::all();
+
+        return view('pemain.index', compact('data_pemain'));
+    }
 }
